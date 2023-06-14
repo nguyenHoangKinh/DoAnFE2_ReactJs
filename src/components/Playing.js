fb272807f3d18adc05e1df57ccce9385f1702484
dung <21211tt4157@mail.tdc.edu.vn>
2023-06-14 20:22:07 +0700

    b

--- src/components/Playing.js
+++ src/components/Playing.js
@@ -2,6 +2,30 @@ import React, { useContext } from "react";
 import AudioPlayer from "react-h5-audio-player";
 import "react-h5-audio-player/lib/styles.css";
 import { Songs } from "../Context";
+import { RHAP_UI } from 'react-h5-audio-player';
+
+class VolumePercentage extends PureComponent<{}, { volumeText: string }> {
+  player = createRef<AudioPlayer>()
+
+  state = { volumeText: '100%' }
+
+  componentDidMount(): void {
+    this.player.current.audio.current.addEventListener('volumechange', (e) => {
+      this.setState({ volumeText: `${((e.target as HTMLAudioElement).volume * 100).toFixed(0)}%` })
+    })
+  }
+
+  render(): React.ReactNode {
+    const { volumeText } = this.state
+    return (
+      <AudioPlayer
+        ref={this.player}
+        src={SAMPLE_MP3_URL}
+        customVolumeControls={[RHAP_UI.VOLUME, <div key={2}>&nbsp;&nbsp;{volumeText}</div>]}
+      />
+    )
+  }
+}
 
 export default function Playing() {
     const {song, handleSetSong} = useContext(Songs)
@@ -16,11 +40,21 @@ export default function Playing() {
       <AudioPlayer
         className="player-music"
         src={song.url}
-        layout="horizontal-reverse"
+        layout="horizontal"
         showSkipControls={true}
         showJumpControls={true}
         onClickNext={handleClickNext}
         onClickPrevious={handleClickPre}
+        customProgressBarSection={
+          [
+            RHAP_UI.CURRENT_TIME,
+            <div>/</div>,
+            RHAP_UI.DURATION,
+            RHAP_UI.PROGRESS_BAR,
+            RHAP_UI.VOLUME,
+          ]
+        }
+        customVolumeControls={[]}
       />
     </div>
   );
