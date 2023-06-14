d4bb1fb78c06aa9b2d4652d10fb0fd759ab8c3b9
nguyenHoangKinh <hoangkinh.tdc2223@gmail.com>
2023-06-15 04:31:44 +0700

    sua cai tieu de cua than dung

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ export default function Navbar() {
     return (
         <div className='h-24 bg-slate-900 text-white text-center leading-[6rem] text-3xl img-anh'>
             <i className='fa fa-eercast mr-4 fa-spin'></i>
-            Nhạc Tịnh Tâm
+            Music Player
         </div>
     )
 }
